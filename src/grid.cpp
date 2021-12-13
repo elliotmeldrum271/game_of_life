f5d748ed240501ba6103917639fa86fa30dd1bf3
userisanapple <coolapple14@gmail.com>
2021-12-12 22:36:32 -0800

    cleaned up grid.cpp and grid.h

--- src/grid.cpp
+++ src/grid.cpp
@@ -13,39 +13,30 @@ Grid::Grid() {
 
 
 void Grid::set_cell(int i, int j, bool is_alive) {
+    // Set value at (i, j) to is_alive
     bit_array[i*NUM_COLS + j] = is_alive;
 }
 
 bool Grid::get_cell(int i, int j) {
+    // Return value at (i, j)
     return bit_array[i*NUM_COLS + j];
 }
 
 bool Grid::in_bounds(int row, int col) {
+    // Return true if (row, cell) is in bounds
     return (row >= 0 && row < NUM_ROWS && col >= 0 && col < NUM_COLS);
 }
 
-void Grid::print_neighbors() {
-
-    std::cout << "in print neighbors\n";
-    for(int i = 0; i <  NUM_ROWS; i++) {
-        for(int j = 0; j < NUM_COLS; j++) {
-            std::cout << get_cell(i, j) << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "end of print neighbors" << std::endl;
-
-}
-
 int Grid::count_neighbors(int row, int col) {
-
-
     int live_neighbors = 0;
+    // Iterate through all 8 adjacent cells
     for(int i = row - 1; i < row + 2; i++) {
         for(int j = col - 1; j < col + 2; j++) {
+            // Make sure it is within the bounds of the grid
             if(in_bounds(i, j)) {
+                // Make sure it is not the cell at (row, cell)
                 if(!(i == row && j == col)) {
+                    // If there is a cell at (i, j), increment live_neighbors
                      if(get_cell(i, j)) {
                         live_neighbors++;
                     }
@@ -54,47 +45,30 @@ int Grid::count_neighbors(int row, int col) {
         }
     }
 
-    //std::cout << "Cell (" << row << ", " << col << ") has " << live_neighbors << " neighbors." << std::endl;
-
     return live_neighbors;
 }
 
 void Grid::update() {
-    // update the grid according to the rules specified by conways game of life
+    // Update the grid based on rules to Conway's Game of Life
     // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
     // Any live cell with two or three live neighbours lives on to the next generation.
     // Any live cell with more than three live neighbours dies, as if by overpopulation.
     // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
     bool* updated_bit_array = new bool[NUM_ROWS * NUM_COLS];
 
-    // std::cout << "count neighbors before" << std::endl;
-    // for(int i = 0; i < NUM_ROWS; i++) {
-    //     for(int j = 0; j < NUM_COLS; j++) {
-    //         std::cout << count_neighbors(i, j) << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
-    //     std::cout << "\nidxs\n" << std::endl;
-    // for(int i = 0; i < NUM_ROWS; i++) {
-    //     for(int j = 0; j < NUM_COLS; j++) {
-    //         std::cout << "(" << i << "," << j <<")" << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-
     for(int i = 0; i < NUM_ROWS; i++) {
         for(int j = 0; j < NUM_COLS; j++) {
             updated_bit_array[i * NUM_COLS + j] = bit_array[i * NUM_COLS + j];
             int live_neighbors = count_neighbors(i, j);
-            if(get_cell(i, j)) { // live
+            // If the cell is alive
+            if(get_cell(i, j)) { 
+                // Set the cell to dead is it does not have 2 or 3 live neighbors
                 if(live_neighbors < 2 || live_neighbors > 3) {
-                    //std::cout << "Cell (" << i << ", " << j << ") is dying with " << live_neighbors << " live neighbors." << std::endl;
-                    //std::cout << "before " << updated_bit_array[i*NUM_COLS + j] << std::endl;
                     updated_bit_array[i*NUM_COLS + j] = false;
-                    //std::cout << "after " << updated_bit_array[i*NUM_COLS + j] << std::endl;
                 }
-            } else { // dead
+            // If the cell is dead
+            } else { 
+                // Set the cell to alive if it has exactly 3 neighbors
                 if(live_neighbors == 3) {
                     updated_bit_array[i*NUM_COLS + j] = true;
                 }
@@ -102,28 +76,10 @@ void Grid::update() {
         }
     }
 
+    // Update the bit array
     bool* old_bit_array = bit_array;
     bit_array = updated_bit_array;
     delete[] old_bit_array;  
-
-    // std::cout << "count neighbors after" << std::endl;
-    // for(int i = 0; i < NUM_ROWS; i++) {
-    //     for(int j = 0; j < NUM_COLS; j++) {
-    //         std::cout << count_neighbors(i, j) << " ";
-    //     }
-    //     std::cout << std::endl;
-    // }
-    // print_neighbors();
-
-    std::cout << get_cell(2, 1) << std::endl;
-
-    // for(int i = 0; i < NUM_ROWS; i++) {
-    //     for(int j = 0; j < NUM_COLS; j++) {
-    //         bit_array[i * NUM_COLS + j] = updated_bit_array[i * NUM_COLS + j];
-    //     }
-    // }
-
-    // delete[] updated_bit_array; 
 }
 
 
--- src/grid.h
+++ src/grid.h
@@ -2,8 +2,8 @@
 #ifndef __GRID_H
 #define __GRID_H
 
-const int NUM_ROWS = 10;
-const int NUM_COLS = 10;
+const int NUM_ROWS = 50;
+const int NUM_COLS = 50;
 
 class Grid {
     private:
@@ -14,7 +14,6 @@ class Grid {
         Grid();
         bool get_cell(int i, int j);
         void set_cell(int i, int j, bool is_alive);
-        void print_neighbors();
         void update();
         ~Grid();
 };
