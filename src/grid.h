3e921eec29ca0450b6e23d2b0df1bbcc8d85068e
userisanapple <coolapple14@gmail.com>
2021-12-12 17:27:14 -0800

    merge conflict 2

--- src/grid.h
+++ src/grid.h
@@ -9,7 +9,6 @@ class Grid {
     private:
         bool* bit_array;
 <<<<<<< HEAD
-        void set_cell(int i, int j, bool is_alive);
         int count_neighbors(int i, int j);
 =======
 >>>>>>> 7aa6bcc07a99a42b1b93ea88c318d32b429160e7
